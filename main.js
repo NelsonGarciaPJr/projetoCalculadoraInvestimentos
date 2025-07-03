bc77611e2fe57fd62948845c45efb469a8d3f247
Nelson Garcia <nelsongarciapjr@gmail.com>
2025-07-03 17:53:58 -0300

    Formulário completado.

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 import { generateReturnsArray } from "./src/investmentGoals.js";
 
 const form = document.getElementById("investment-form");
+const clearFormButton = document.getElementById("clear-form");
 // const calculateButton = document.getElementById("calculate-results");
 
 function renderProgression(event) {
   event.preventDefault();
+  if (document.querySelector(".error")) {
+    return;
+  }
   //   const startingAmount = Number(form["starting-amount"].value);
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
-  const timeAmount = Number(document.getElementById("time-amount").value);
+  const timeAmount = Number(
+    document.getElementById("time-amount").value.replace(",", ".")
+  );
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -30,5 +40,57 @@ function renderProgression(event) {
   console.log(returnsArray);
 }
 
+function clearForm() {
+  form["starting-amount"].value = "";
+  form["additional-contribution"].value = "";
+  form["time-amount"].value = "";
+  form["return-rate"].value = "";
+  form["tax-rate"].value = "";
+  //
+  const errorInputContainers = document.querySelectorAll(".error");
+  for (const errorInputContainer of errorInputContainers) {
+    errorInputContainer.classList.remove("error");
+    errorInputContainer.parentElement.querySelector("p").remove();
+  }
+}
+
+function validateInput(event) {
+  //   console.log(event.target);
+  if (event.target.value === "") {
+    return;
+  }
+  //   Quando utilizar { parentElement }, não precisa colocar o .parentElement no final, pois tem mesmo nome
+  const { parentElement } = event.target;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(",", ".");
+
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue)) <= 0
+  ) {
+    // OBJETIVO: <p class="text-red-500">Insira um valor númerico e maior que zero.</p>
+    const errorTextElement = document.createElement("p"); // <p><p>
+    errorTextElement.classList.add("text-red-500"); // <p class='text-red-500'><p>
+    errorTextElement.innerText = "Insira um valor númerico e maior que zero."; // <p class="text-red-500">Insira um valor númerico e maior que zero.</p>
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
 form.addEventListener("submit", renderProgression);
 // calculateButton.addEventListener("click", renderProgression);
+clearFormButton.addEventListener("click", clearForm);
